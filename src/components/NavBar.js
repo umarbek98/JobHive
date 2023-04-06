5ec2a90d65bdd754602551f8f2fafa8fb7852eb4
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-04-06 12:38:14 -0400

    added login front end side and register

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button, Modal } from 'react-bootstrap';
 import styles from './NavBar.module.css'
 import { Link } from 'react-router-dom';
 
-function NavBar({ what, where, setWhat, setWhere, setCurrJob}) {
+function NavBar({ what, where, setWhat, setWhere, setCurrJob }) {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
   function handleSearch(event) {
     event.preventDefault();
     setWhat(event.target.what.value)
@@ -12,6 +15,23 @@ function NavBar({ what, where, setWhat, setWhere, setCurrJob}) {
 
   function handleClick(e){
     setCurrJob(null)
+  }
+
+  function handleLogin() {
+    setShowLogin(true);
+  }
+
+  function handleCloseLogin() {
+    setShowLogin(false);
+  }
+
+  function handleRegister() {
+    setShowRegister(true);
+    setShowLogin(false);
+  }
+
+  function handleCloseRegister() {
+    setShowRegister(false);
   }
 
   return (
@@ -30,9 +50,66 @@ function NavBar({ what, where, setWhat, setWhere, setCurrJob}) {
                 <Button type="submit">Search</Button>
             </div>
         </Form>
+        <Button className={styles.loginButton} variant="primary" onClick={handleLogin}>Login</Button>
       </Navbar.Collapse>
+      {/* login modal */}
+      <Modal show={showLogin} onHide={handleCloseLogin}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseLogin}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleRegister}>
+            Don't have an account?
+          </Button>
+          <Button variant="primary" onClick={handleCloseLogin}>
+            Login
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {/* register modal */}
+      <Modal show={showRegister} onHide={handleCloseRegister}>
+        <Modal.Header closeButton>
+          <Modal.Title>Register</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseRegister}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleCloseRegister}>
+            Register
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Navbar>
   );
 }
-
 export default NavBar;
