26c747a1fcab6760f578a0b1ce3ca35cba27e743
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-04-23 17:12:49 -0400

    removed images

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -3,34 +3,12 @@ import styles from "./JobCard.module.css"
 import moment from "moment";
 
 function JobCard({ job, setCurrJob, setJobCardModal, data}){
-    const [companyImage, setCompanyImage] = useState(null);
     function handleclick(e){
         setCurrJob(job)
         setJobCardModal(true)
         // console.log(job)
     }
 
-
-  useEffect(() => {
-    // Search for company image using Pexels API
-    const apiKey = data.REACT_APP_PEXEL
-    const query = job.title;
-    const url = `https://api.pexels.com/v1/search?query=${query}&per_page=1`;
-    fetch(url, {
-      headers: {
-        Authorization: apiKey,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.photos.length > 0) {
-          setCompanyImage(data.photos[0].src.medium);
-        }
-      })
-      .catch((error) => {
-        console.log("Error searching for company image:", error);
-      });
-  }, [job.company.display_name]);
 
 
     return(
@@ -40,13 +18,6 @@ function JobCard({ job, setCurrJob, setJobCardModal, data}){
                 <h3 className={styles.title}>{job.company.display_name}</h3>
                 <p>{job.location.display_name} | {moment(job.created).utc().format('MM/DD/YYYY')}</p>
                 <p>Salary: Estimated ${job.salary_min} to ${job.salary_max}</p>
-                {companyImage && (
-          <img
-            src={companyImage}
-            alt={job.company.display_name}
-            className={styles.companyImage}
-          />
-        )}
             </div>
         </div>
     );
